3fd9e3ce71131ca9129234243ef259b887904e24
D@reK@r@n18 <xpresskaran98@gmail.com>
2023-12-17 00:34:53 +0530

    changes in files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,19 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default () => {
   return defineConfig({
     server: {
-      host: "0.0.0.0",
-      port: "5500",
-      strictPort: true,
-      // proxy: {
-      //   "/api": {
-      //     target: process.env.VITE_API_BASE_URL,
-      //     changeOrigin: true,
-      //     rewrite: (path) => path.replace(/^\/api/, process.env.VITE_API_BASE_URL + "/api"),
-      //   },
-      // },
+      port: process.env.PORT || "5500",
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) =>
+            path.replace(/^\/api/, process.env.VITE_API_BASE_URL + "/api"),
+        },
+      },
     },
     plugins: [react()],
   });
