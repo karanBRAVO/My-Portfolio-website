e65ea098ef4c889bacd0d51273ea43522bfc3c15
D@reK@r@n18 <xpresskaran98@gmail.com>
2023-12-17 00:10:03 +0530

    update vite config and vercel json files

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,13 +8,13 @@ export default ({ mode }) => {
       host: "0.0.0.0",
       port: "5500",
       strictPort: true,
-      proxy: {
-        "/api": {
-          target: process.env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, process.env.VITE_API_BASE_URL + "/api"),
-        },
-      },
+      // proxy: {
+      //   "/api": {
+      //     target: process.env.VITE_API_BASE_URL,
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, process.env.VITE_API_BASE_URL + "/api"),
+      //   },
+      // },
     },
     plugins: [react()],
   });
